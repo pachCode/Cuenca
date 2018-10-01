8616e86f05ff9ac71ba116af6bdd71dbbc59cfe4
pachCode <yohopa@hotmail.com>
2018-09-30 21:40:00 -0500

    Librerias

--- EightQueen.py
+++ EightQueen.py
@@ -2,14 +2,18 @@ import sys
 import os
 import Dao
 
+
 queenNumber = 8
 solutions =[]
 
+# función recursiva
 def find_solution(column, columns,lowerDiagonal, higherDiagonal):
     if column == queenNumber:
+        # Guarda los datos en BD
         Dao.add_solution(solutions)    
     else:
         for j in range(1,queenNumber+1):
+            # Si no esta en ninguna fila, columna o diagonal
             if not j in columns and not j-column in lowerDiagonal and not j+column in higherDiagonal:
                 solutions[column] = j
                 columns.append(j),lowerDiagonal.append(j-column),higherDiagonal.append(j+column)
@@ -17,6 +21,7 @@ def find_solution(column, columns,lowerDiagonal, higherDiagonal):
                 columns.pop(), lowerDiagonal.pop(),higherDiagonal.pop()
 
 def get_solution(number):
+    # Elimina los datos anteriores de la BD
     Dao.romove_results()
     global queenNumber 
     global solutions 
@@ -24,6 +29,7 @@ def get_solution(number):
     solutions= [None]*queenNumber
     find_solution(0,[],[],[])
     numberSolution = Dao.get_solutions_number(queenNumber)
+    # Imprime el número de soluciones
     print(str(numberSolution)+" soluciones")
     return numberSolution
 
